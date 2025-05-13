c92c006995e7c6f85c80ba6903d7d56f958e05ab
Ad4m-eve <len3500tab@gmail.com>
2025-05-13 16:12:08 +0300

    Додала зміни до перевірки заголовків, файли .properties та документи для перевірки

---
+++ src/test/java/csit/semit/semitchecker/TitlesCheckTest.java
@@ -0,0 +1,44 @@
+package csit.semit.semitchecker;
+
+import csit.semit.semitchecker.errorschecking.*;
+import org.apache.poi.xwpf.usermodel.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Locale;
+
+public class TitlesCheckTest {
+    private String enDoc = "titles_layout_text_en.docx";
+    private String uaDoc = "titles_layout_text_ua.docx";
+
+    @Test
+    void testCheckTitleNamesEn() throws IOException {
+        System.out.println("Checking title headings in english");
+        Path path = Paths.get(enDoc);
+        XWPFDocument document = new XWPFDocument(Files.newInputStream(path));
+        CheckParams params = new CheckParams();
+        params.setLocaleDoc(Locale.forLanguageTag("en"));
+        params.setLocaleWord(Locale.forLanguageTag("uk"));
+        ErrorsList errors = new ErrorsTitlesCheck().check(document, params, "title");
+        for (CheckError error : errors.getErrors()) {
+            System.out.println(error);
+        }
+    }
+
+    @Test
+    void testCheckTitleNamesUa() throws IOException {
+        System.out.println("Checking title headings in ukrainian");
+        Path path = Paths.get(uaDoc);
+        XWPFDocument document = new XWPFDocument(Files.newInputStream(path));
+        CheckParams params = new CheckParams();
+        params.setLocaleDoc(Locale.forLanguageTag("uk"));
+        params.setLocaleWord(Locale.forLanguageTag("uk"));
+        ErrorsList errors = new ErrorsTitlesCheck().check(document, params, "title");
+        for (CheckError error : errors.getErrors()) {
+            System.out.println(error);
+        }
+    }
+}
